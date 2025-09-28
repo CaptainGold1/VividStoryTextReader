4fb098329210e3aa899a4f125a82c32ec1cf190c
CaptainGold1 <48659572+CaptainGold1@users.noreply.github.com>
2025-09-28 03:29:01 -0500

    Loading skeletons look good on mobile now

--- src/app/viewer/[game]/[story]/[file]/loading.tsx
+++ src/app/viewer/[game]/[story]/[file]/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
 	return (
 		<div className="flex flex-col items-center w-full h-[calc(100vh-48px)]">
 			<LoadingNavBar/>
-			<div className="flex flex-col sm:w-8/12 gap-1 grow-0 overflow-hidden">
+			<div className="flex flex-col w-11/12 sm:w-8/12 gap-1 grow-0 overflow-hidden">
 				<div className={
 					"mt-8 mb-4 h-[32px] w-1/2 self-center " +
 					"rounded-full loading-skeleton opacity-20 shrink-0"
--- src/utils/getFileData.ts
+++ src/utils/getFileData.ts
@@ -4,7 +4,7 @@ import type {Episode} from "@/types/episode";
 
 export async function getFileData(gameName: string, storyName: string, fileName: string): Promise<Episode> {
 	// Uncomment to add a delay to test loading skeletons.
-	// await new Promise(r => setTimeout(r, 1000));
+	// await new Promise(r => setTimeout(r, 5000));
 
 	return new Promise<Episode>((resolve, reject) => {
 		fs.readFile(`src/data/${gameName}/${storyName}/${fileName}.json`, (err, data) => {
